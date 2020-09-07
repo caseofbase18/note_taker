d7b8a560e94dda1f9dd19f4f3e4e2b7f4e1a3265
Casey Maltese <caseymaltese@gmail.com>
2020-09-07 18:07:25 -0400

    modified server.js

--- server.js
+++ server.js
@@ -4,16 +4,18 @@ const app=express();
 
 //app.get
 app.get("/notes", (request,response)=>{
-    fs.readFile("./public/notes.html", "utf8", (error,data)=>{
-        response.send(data);
+    // fs.readFile("./public/notes.html", "utf8", (error,data)=>{
+    //     response.send(data);
+    response.sendFile(__dirname+"/public/notes.html");
     });  
-});
+// });
 
 app.get("*", (request, response)=>{
-    fs.readFile("./public/index.html", "utf8", (error,data)=>{
-        response.send(data);
+    // fs.readFile("./public/index.html", "utf8", (error,data)=>{
+    //     response.send(data);
+    response.sendFile(__dirname+"/public/index.html");
     });
-});
+// });
 
 app.listen(8000,()=>{
     console.log("Listening to server 8000");
