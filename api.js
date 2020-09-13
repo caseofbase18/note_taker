796c43152f8ecbe1eed5f1beabe25dd4e96faa16
Casey Maltese <caseymaltese@gmail.com>
2020-09-13 14:21:58 -0400

    delete routes est

--- api.js
+++ api.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const path = require("path");
 const fs = require("fs");
 const notes = require("./db/db.json");
+const uuid = require("uuid/v1");
 
 
 router.get("/api/notes", function (req, res) {
@@ -11,6 +12,7 @@ router.get("/api/notes", function (req, res) {
 
 router.post("/api/notes", function (req, res) {
     let newNote = req.body;
+    newNote.id = uuid();
     notes.push(newNote);
     console.log(notes);
     fs.writeFile("./db/db.json", JSON.stringify(notes), function (error, data) {
@@ -21,14 +23,15 @@ router.post("/api/notes", function (req, res) {
 });
 
 router.delete("/api/notes/:id", function (req, res) {
-    for(let i=0; i < notes.length; i++) {
-        if (req.params.id == notes[i].id){
-            notes.splice(i,1)
-        } 
+    for (let i = 0; i < notes.length; i++) {
+        if (req.params.id == notes[i].id) {
+            notes.splice(i, 1)
+        }
     }
     fs.writeFile("./db/db.json", JSON.stringify(notes), function (error, data) {
         if (error) throw error
         res.send("note deleted");
+    });
 });
-});
+
 module.exports = router;
