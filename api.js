656f8de95f5398ecc18718354d45c4a02d1ca00d
Casey Maltese <caseymaltese@gmail.com>
2020-09-13 13:43:48 -0400

    get and post routes static

---
+++ api.js
@@ -0,0 +1,27 @@
+const router = require("express").Router();
+const path = require("path");
+const fs = require("fs");
+const notes = require("./db/db.json");
+
+
+router.get("/api/notes", function (req, res) {
+    console.log("i'm the get route")
+    return res.json(notes);
+});
+
+router.post("/api/notes", function (req, res) {
+    let newNote = req.body;
+    notes.push(newNote);
+    console.log(notes);
+    fs.writeFile("./db/db.json", JSON.stringify(notes), function (error, data) {
+        if (error) throw error
+        console.log("Your file is created!")
+        res.json(newNote);
+    });
+});
+
+router.delete("/api/notes/:id", function (req, res) {
+    res.send(req.params)
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,23 +1,42 @@
-const express=require("express");
-const fs=require("fs");
-const app=express();
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+const routes = require("./api.js");
+
+//handles POST requests
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+app.use("/", routes);
 
 //app.get
-app.get("/notes", (request,response)=>{
+app.get("/notes", (request, response) => {
     // fs.readFile("./public/notes.html", "utf8", (error,data)=>{
     //     response.send(data);
-    response.sendFile(__dirname+"/public/notes.html");
-    });  
+    response.sendFile(__dirname + "/public/notes.html");
+});
 // });
 
-app.get("*", (request, response)=>{
+app.get("*", (request, response) => {
     // fs.readFile("./public/index.html", "utf8", (error,data)=>{
     //     response.send(data);
-    response.sendFile(__dirname+"/public/index.html");
-    });
+    response.sendFile(__dirname + "/public/index.html");
+});
 // });
 
-app.listen(8000,()=>{
-    console.log("Listening to server 8000");
+//Listener
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "notes.html"));
+});
+
+app.listen(PORT, () => {
+    console.log("Listening on port 8000");
 });
 
